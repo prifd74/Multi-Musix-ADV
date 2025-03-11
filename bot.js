84a639ca1f82a34269acb9cb052afb6c573f5699
prifd74 <prifdgamer@gmail.com>
2025-03-11 22:57:11 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,9 @@ const { Classic } = require("musicard");
 const { prefix } = require('./config.json');
 const nodes = [
     {
-        host: "37.114.42.191",
-        port: 9906, 
-        password: "danteisnttaken", 
+        host: "180.188.226.76",
+        port: 7019, 
+        password: "enteryourcustompass", 
          secure: false
       },
 ];
